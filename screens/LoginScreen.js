fdc827240f34c7ed8a91b96bd3aae10b6cedf0aa
shyamtiwari02062001 <shyam02062001@gmail.com>
2022-09-21 04:30:12 +0530

    handeled errors

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,11 +2,19 @@ import AuthContent from '../components/Auth/AuthContent';
 import React, {useState} from 'react';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import {login} from '../util/auth';
+import {Alert} from 'react-native';
 function LoginScreen() {
   const [loading, setLoading] = useState(false);
   const signInHandler = async ({email, password}) => {
     setLoading(true);
-    await login(email, password);
+    try {
+      await login(email, password);
+    } catch (err) {
+      Alert.alert(
+        'Authentication Failed !',
+        'Could not log you in. Please check your credential',
+      );
+    }
     setLoading(false);
   };
   if (loading) {
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -2,11 +2,19 @@ import AuthContent from '../components/Auth/AuthContent';
 import React, {useState} from 'react';
 import {createUser} from '../util/auth';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
+import {Alert} from 'react-native';
 function SignupScreen() {
   const [loading, setLoading] = useState(false);
   const signUpHandler = async ({email, password}) => {
     setLoading(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch (error) {
+      Alert.alert(
+        'Authentication Failed !',
+        'Could not log you in. Please check your credential and try agin later !',
+      );
+    }
     setLoading(false);
   };
   if (loading) {
